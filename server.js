e56616a70703432bc7c85b08bf1ba0abe098e760
CONTINI Baptiste <36010397+Enta12@users.noreply.github.com>
2022-07-29 15:48:50 +0200

    fix(cors): use cors package

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import charactersRouter from './routes/characters.js';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 
 //Routes
@@ -23,15 +24,9 @@ app.use(express.json())
 app.use('/characters', charactersRouter);
 
 //Permet les requêtes cross-origin
-//Permet de dire au navigateur que les requêtes sont OK (pas pirate) et ne pas faire d'erreurs CORS
-app.use((req, res, next) => {
-    //Accède à l'API depuis n'importe quelle origine
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    //Ajoute des headers à notre API
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+app.use(cors({
+  origin: 'https://lysande.pepintrie.fr'
+}));
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
