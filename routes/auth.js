1eec4f5d4d1a992df566ed5ee2c00a22301b674a
CONTINI Baptiste <36010397+Enta12@users.noreply.github.com>
2022-08-08 11:54:48 +0200

    feat(register): only for admin

--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,11 @@ import express from 'express';
 import {
   signup, login,
 } from '../controllers/auth.js';
+import auth from '../middlewares/auth.js';
 
 const router = express.Router();
 
-router.post('/signup', signup);
+router.post('/signup', auth, signup);
 router.post('/login', login);
 
 export default router;
