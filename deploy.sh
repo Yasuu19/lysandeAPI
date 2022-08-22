13da03db379c56416eefb8aaf656385f6236a0a9
CONTINI Baptiste <36010397+Enta12@users.noreply.github.com>
2022-08-22 10:44:46 +0200

    revert(availability): for investigate

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ git pull --rebase
 docker stop lysandeapi
 docker rm lysandeapi
 docker build -t lysandeapi .
-docker run -it --name lysandeapi --ip 172.18.0.31 --network petit lysandeapi
+docker run -dit --name lysandeapi --ip 172.18.0.31 --network petit lysandeapi
