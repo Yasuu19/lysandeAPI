593a5e06ac82ef77fd51970f91991502b3720e55
CONTINI Baptiste <36010397+Enta12@users.noreply.github.com>
2022-09-29 16:14:21 +0200

    feat(session): delete session

--- routes/session.js
+++ routes/session.js
@@ -2,7 +2,7 @@ import express from 'express';
 import auth from '../middlewares/auth.js';
 
 import {
-  getSessions, createSession, getSession, updateSession,
+  getSessions, createSession, getSession, updateSession, deleteSession,
 } from '../controllers/session.js';
 
 const router = express.Router();
@@ -11,5 +11,6 @@ router.post('/', auth, createSession);
 router.get('/', auth, getSessions);
 router.get('/:id', auth, getSession);
 router.put('/:id', auth, updateSession);
+router.delete('/:id', auth, deleteSession);
 
 export default router;
